d75ad9c37128441ecd260af7a76b4f22900d9671
jbachurski <kbachurski@gmail.com>
2019-11-24 15:19:34 +0100

    Tests, colors, better multiprocessing, diffs, and various other things

--- taucheck.py
+++ taucheck.py
@@ -2,7 +2,9 @@ from pathlib import Path
 import multiprocessing
 import collections
 import subprocess
+import difflib
 import filecmp
+import pprint
 import random
 import string
 import time
@@ -12,14 +14,26 @@ import os
 try:
     from tqdm import tqdm
 except ImportError as e:
-    def tqdm(it):
-        it = list(it)
-        def p(i): return round(i/len(it)*100)
+    def tqdm(it, total=None):
+        if total is None:
+            total = it.__length_hint__()
+        def p(i): return round(i/total*100)
         for i, x in enumerate(it):
             if p(i) != p(i-1):
                 print("{: >3}%...".format(p(i)), end="\r")
             yield x
         print("100%... done")
+
+try:
+    import colorama
+    colorama.init()
+    Fore, Back, Style = colorama.Fore, colorama.Back, colorama.Style
+except ImportError as e:
+    class Fakerama:
+        def __getattribute__(self, attr):
+            return ''
+    Fore, Back, Style = Fakerama(), Fakerama(), Fakerama()
+
 import click
 
 # Temporary files: a decorated function receives an additional parameter,
@@ -36,6 +50,19 @@ def with_temporary_file(func, suffix='.tmp', dlen=4):
         finally:
             os.remove(tmp)
     return wrapped
+
+
+def result_diff(expected, got):
+    a, b = expected.splitlines(), got.splitlines()
+    s = difflib.SequenceMatcher(None, a, b)
+    r = ''
+    for tag, i1, i2, j1, j2 in s.get_opcodes():
+        if tag == 'equal':
+            continue
+        r += '{:7}   a[{}:{}] --> b[{}:{}] {!r:>8} --> {!r}\n'.format(
+            tag, i1, i2, j1, j2, a[i1:i2], b[j1:j2]
+        )
+    return r
 
 
 # Verifiers are objects that take care of testing singular cases.
@@ -67,20 +94,22 @@ class Verifier:
         # got is a temporary file for program output
         try:
             start = time.time()
-            subprocess.run(
-                self.app, shell=True, timeout=self.timeout,
-                stdin=open(self.input_of(case), 'rb'), stdout=open(got, 'wb'),
+            process = subprocess.run(
+                self.app, shell=True, timeout=2*self.timeout, check=True,
+                stdin=open(self.input_of(case), 'rb'), stdout=open(got, 'wb')
             )
             end = time.time()
         except subprocess.TimeoutExpired as e:
-            return VerifyStatus(None, self.timeout, case, {'timeout': True})
+            return VerifyStatus(None, 2*self.timeout, case, {'timeout': True, 'code': None})
+        except subprocess.CalledProcessError as e:
+            return VerifyStatus(None, round(time.time() - start, 6), case, {'timeout': False, 'code': e.returncode})
 
-        meta = {'timeout': False}
+        meta = {'timeout': end-start > self.timeout, 'code': process.returncode}
         v = self.verify(case, got)
         # In the simple case, verify returns only the status (`ok`). Otherwise,
         # it is a dictionary containing the status and metadata.
         if isinstance(v, dict):
-            meta = v
+            meta.update(v)
             del meta['ok']
             v = v['ok']
 
@@ -89,14 +118,23 @@ class Verifier:
 
 class IdenticalOutputVerifier(Verifier):
     def verify(self, case, got):
-        return filecmp.cmp(got, self.output_of(case))
+        if filecmp.cmp(got, self.output_of(case)):
+            return True
+        else:
+            a = open(self.output_of(case)).read()
+            b = open(got).read()
+            return {'ok': False, 'diff': result_diff(a, b)}
 
 
 class LooseOutputVerifier(Verifier):
     def verify(self, case, got):
-        a = open(got).read()
-        b = open(self.output_of(case)).read()
-        return a.split() == b.split()
+        a = open(self.output_of(case)).read()
+        b = open(got).read()
+        if a.split() == b.split():
+            return True
+        else:
+            diff = difflib.context_diff(a, b, fromfile='expected', tofile='got')
+            return {'ok': False, 'diff': result_diff(a, b)}
 
 # TODO: test this
 class CheckerVerifier(Verifier):
@@ -104,8 +142,8 @@ class CheckerVerifier(Verifier):
         param = [self.checker, self.input_of(case), got]
         if self.out_path is not None:
             param.insert(2, self.output_of(case))
-        process = subprocess.run(param, shell=True, timeout=self.timeout)
-        return {'ok': process.returncode == 0, 'code': process.returncode}
+        process = subprocess.run(param, shell=True, timeout=self.timeout, capture_output=True)
+        return {'ok': process.returncode == 0, 'checkcode': process.returncode, 'comment': process.stdout.decode()}
 
 
 available_verifiers = {
@@ -117,14 +155,14 @@ available_verifiers = {
 
 # Input filenames are sorted using these.
 
+def tryint(x):
+    try:
+        return int(x)
+    except:
+        return x
+def nkey(x):
+    return tuple(tryint(c) for c in re.split('([0-9]+)', str(x)))
 def natural_sort(it):
-    def tryint(x):
-        try:
-            return int(x)
-        except:
-            return x
-    def nkey(x):
-        return tuple(tryint(c) for c in re.split('([0-9]+)', str(x)))
     return sorted(it, key=nkey)
 
 def shuffled(it):
@@ -168,21 +206,38 @@ def find_prefixwise(it, string):
     return dec[-1][1]
 
 # Handles the verifier doing all the work.
-def verify_job(inputs, verifier, verbose):
-    stati = []
-    for i in inputs:
-        status = verifier(i.stem)
-        stati.append(status)
-        if verbose > 0:
-            print(status)
-        if verbose < 0:
-            # TODO: multiprocessing progress bar
-            pass
-    return stati
+def verify_job(cases, verifier, verbose):
+    for i in cases:
+        status = verifier(i)
+        yield status
 
 # Version for multiprocessing (ignore verbose flag, logging is messed up anyway)
-def verify_job_mp(args):
-    return verify_job(*args, -1)
+
+process_verifier = None
+def initialize_worker(vertype, param):
+    global process_verifier
+    process_verifier = vertype(*param)
+
+def verify_single(case):
+    return process_verifier(case)
+
+
+def test_status_acronym(status):
+    ret = None
+    form = ""
+    if status.ok is True:
+        ret, form = " OK", Fore.GREEN
+    elif status.ok is False:
+        ret, form = " WA", Fore.RED
+    else:
+        if status.meta['timeout']:
+            ret = "TLE"
+        else:
+            ret = " ??"
+            form = Fore.MAGENTA + Style.DIM
+    if status.meta['timeout']:
+        form = Fore.YELLOW + Style.BRIGHT
+    return form + ret + Style.RESET_ALL
 
 # TODO: handle wrapper callers like `oiejq`
 @click.command()
@@ -194,11 +249,14 @@ def verify_job_mp(args):
 @click.option('-c', '--checker', default=None, help="Checker program. Should take input filename, expected out filename and received out filename as arguments and return a nonzero status code if the output is not correct.")
 @click.option('-t', '--timeout', default=600, type=float, help="Program timeout length.")
 @click.option('-p', '--processes', default=1, type=int, help="Enable multiprocessing with given process count.")
+@click.option('-f', '--fatal', default=False, is_flag=True, help="Stop testing when a non-ok status is encountered.")
 @click.option('-v', '--verbose', default=0, count=True, help="Controls verbosity level.")
-def main(app, tests, outputs, s_order, s_verify, checker, timeout, processes, verbose):
+def main(app, tests, outputs, s_order, s_verify, checker, timeout, processes, fatal, verbose):
     """
     Test the program APP using test cases provided in the TESTS directory.
     """
+
+    start = time.time()
 
     s_verify = find_prefixwise(available_verifiers.keys(), s_verify)
     s_order = find_prefixwise(available_orderings.keys(), s_order)
@@ -206,38 +264,73 @@ def main(app, tests, outputs, s_order, s_verify, checker, timeout, processes, ve
     order = available_orderings[s_order]
 
     in_path = Path(tests)
-    out_path = Path(outputs if outputs is not None else tests)
+    if outputs is not None or s_verify != 'checker':
+        out_path = Path(outputs if outputs is not None else tests)
+    else:
+        out_path = None
 
     if checker and s_verify != 'checker':
-        print('[?] Checker parameter provided but verifier is not checker.')
+        print("[?] Checker parameter provided but verifier is not checker.")
 
     verifier_params = (app, in_path, out_path, checker, timeout)
 
     inputs = order(in_path.glob('*.in'))
+    cases = [i.stem for i in inputs]
 
+    stati = []
+    job = None
+    pool = None
+
+    # Prepare job
     if processes == 1:
-        # Maybe enable progress bar
-        if not verbose:
-            inputs = tqdm(inputs)
-        stati = verify_job(inputs, verifier_t(*verifier_params), verbose)
+        job = verify_job(cases, verifier_t(*verifier_params), verbose)
     else:
-        with multiprocessing.Pool(processes) as pool:
-            # Split the tests in regular strides and create verifiers
-            data = [(inputs[start::processes], verifier_t(*verifier_params)) for start in range(processes)]
-            stati = sum(pool.map(verify_job_mp, data), [])
+        # A worker gets a verifier instance, so we don't
+        # recreate it needlessly
+        def winit():
+            return initialize_worker(verifier_t, verifier_params)
+        pool = multiprocessing.Pool(processes, initializer=winit)
+        job = pool.imap_unordered(verify_single, cases)
+
+    job = tqdm(job, total=len(cases))
+
+    for status in job:
+        if not status.ok and fatal:
+            print("!@#")
+            break
+        stati.append(status)
+
+    if pool is not None:
+        pool.close()
 
     correct = sum(bool(s.ok) for s in stati)
     total = len(stati)
     print("Correct: {}/{} ({}%)".format(correct, total, round(correct/total*100, 1)))
 
-    if correct == total:
-        print("AC! :)")
-    else:
-        # TODO: better output formatting
-        for s in stati:
-            if not s.ok:
-                print(s)
+    stati.sort(key=lambda s: nkey(s.case))
 
+    if correct == total:
+        print(Fore.GREEN + Style.BRIGHT + "AC! :)" + Style.RESET_ALL)
+    else:
+        print("===\nTest summary\n===")
+        for s in stati:
+            if not verbose and s.ok:
+                continue
+            code = s.meta['code'] if 'code' in s.meta and s.meta['code'] is not None else '??'
+            print("- {} {} ({: >6.3f}s) -> {}".format(test_status_acronym(s), s.case, s.time, code))
+            if verbose:
+                if 'diff' in s.meta:
+                    if len(s.meta['diff']) > 512:
+                        print("[..diff too long, snip..]")
+                    else:
+                        pprint.pprint(s.meta['diff'])
+                if 'comment' in s.meta:
+                    print("Checker comment:")
+                    print(s.meta['comment'])
+
+    end = time.time()
+
+    print("Done in {:.3f}s".format(end - start))
 
 
 if __name__ == "__main__":
